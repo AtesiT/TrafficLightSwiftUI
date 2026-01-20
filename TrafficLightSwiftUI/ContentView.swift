8b2b9623926baee6caa24c5b185228e7affe629a
AtesiT <atesit@mail.ru>
2026-01-20 10:47:43 +0300

    ADD: func to changeColor

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -1,20 +1,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var redOpacity = 0.1
+    private var yellowOpacity = 0.1
+    private var greenOpacity = 0.1
+    
+    private var currentColor = "none"
+    
     var body: some View {
         VStack {
             Circle()
                 .foregroundStyle(Color.red)
+                .opacity(redOpacity)
             Circle()
                 .foregroundStyle(Color.yellow)
+                .opacity(yellowOpacity)
             Circle()
                 .foregroundStyle(Color.green)
+                .opacity(greenOpacity)
             Button("START") {
                 return
             }
 
         }
         .padding()
+    }
+    
+    private mutating func changeColor() {
+        switch currentColor {
+        case "red":
+            redOpacity = 0.1
+            currentColor = "yellow"
+            yellowOpacity = 1.0
+        case "yellow":
+            yellowOpacity = 0.1
+            currentColor = "green"
+            greenOpacity = 1.0
+        default:
+            greenOpacity = 0.1
+            currentColor = "red"
+            redOpacity = 1.0
+        }
     }
 }
 
